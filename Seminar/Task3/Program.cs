7ee8b659aa412b41577c2b943d10391f1a8df950
alex_rc6 <sasha.rc6@gmail.com>
2024-03-14 23:37:51 +0500

    Added Tasks 3-4

---
+++ Seminar/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int a1 = Num % 10;
+int a2 = Num / 10 % 10;
+int a3 = Num / 100;
+
+int sum = a1 + a3;
+Console.WriteLine($"Сумма первого и последнего чисел {a1} и {a3}: {sum}");
---
+++ Seminar/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string n = Console.ReadLine()!;
+
+if (n.Length != 3)
+{
+    Console.WriteLine("Это не трехзначное число...");
+}
+else
+{
+    Console.WriteLine(Convert.ToString(n[0]) + Convert.ToString(n[2]));
+}
+// int a1 = Num % 10;
+// int a2 = Num / 10 % 10;
+// int a3 = Num / 100;
+
+// int sum = a1 + a3;
+//Console.WriteLine($"Сумма первого и последнего чисел {a1} и {a3}: {sum}");
