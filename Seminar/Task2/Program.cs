0af73fc3e5182dc65335a0a8e19b1ed410213c56
alex_rc6 <sasha.rc6@gmail.com>
2024-03-14 22:53:14 +0500

    Added programm 1 and 2

---
+++ Seminar/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int notN = -N;
+while(notN <= N)
+{
+    Console.Write($"{notN} ");
+    notN++;
+}
+
