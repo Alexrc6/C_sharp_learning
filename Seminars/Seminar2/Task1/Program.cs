c3cf8966609ce65c77f79e85cd29aa551bd21487
alex_rc6 <sasha.rc6@gmail.com>
2024-03-18 22:25:56 +0500

    Solved Task2 Seminar 2

--- Seminars/Seminar2/Task1/Program.cs
+++ Seminars/Seminar2/Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int a = num % 10;
+int b = num / 100;
+int num2 = b*10 + a;
+
+Console.WriteLine("Первое число равно {0}, второе - {1}, новое число равно {2}", a, b, num2);
+//Console.WriteLine(num2);
