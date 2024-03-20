05e6bc14acd4821195a242eefa277ca5ef810171
alex_rc6 <sasha.rc6@gmail.com>
2024-03-20 21:43:31 +0500

    Solved HomeWork:ToSeminar2:Task1

---
+++ HomeWork/ToSeminar2/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая принимает
+//  на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+//Console.WriteLine("Введите целое число");
+//int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите любое число");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine($"Число {num} кратно одновременно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно одновременно 7 и 23");
+}
+
+
+
+
