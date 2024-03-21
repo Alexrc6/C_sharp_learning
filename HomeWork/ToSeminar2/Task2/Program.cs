1feae38f07bc683bf57bb1694d3ffae9d55b589c
alex_rc6 <sasha.rc6@gmail.com>
2024-03-21 22:05:25 +0500

    HW.ToSem2:Task2. Tried the Split input option as in the example

--- HomeWork/ToSeminar2/Task2/Program.cs
+++ HomeWork/ToSeminar2/Task2/Program.cs
@@ -9,35 +9,48 @@ class Program
     static void Main()
     {
 
-        Console.WriteLine("Введите координату точки X (Х ≠ 0)");
-        Double varX = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine("Введите координату точки X (Х ≠ 0)");
+        // Double varX = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Введите координату точки Y (Y ≠ 0)");
-        Double varY = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine("Введите координату точки Y (Y ≠ 0)");
+        // Double varY = Convert.ToDouble(Console.ReadLine());
+        float varX = 0;
+        float varY = 0;
+
+        Console.WriteLine("Введите координаты X и Y (не равные 0) через пробел");
+        string[] coordinates = Console.ReadLine().Split(' ');
+
+        // if (coordinates is not null)
+        // {
+        //     varX = Convert.ToInt32(coordinates[0]);
+        //     varY = Convert.ToInt32(coordinates[1]);
+        // }
+
+        varX = Convert.ToInt32(coordinates[0]);
+        varY = Convert.ToInt32(coordinates[1]);
+
 
         if (varX == 0 || varY == 0)
         {
             Console.WriteLine("Нарушено условие: Х и Y ≠ 0");
         }
+        else if (varX > 0 && varY > 0)
+        {
+            Console.WriteLine("Номер координатной четверти: I");
+        }
+        else if (varX < 0 && varY > 0)
+        {
+            Console.WriteLine("Номер координатной четверти: II");
+        }
+        else if (varX < 0 && varY < 0)
+        {
+            Console.WriteLine("Номер координатной четверти: III");
+        }
         else
         {
-            if (varX > 0 && varY > 0)
-            {
-                Console.WriteLine("Номер координатной четверти: I");
-            }
-            if (varX < 0 && varY > 0)
-            {
-                Console.WriteLine("Номер координатной четверти: II");
-            }
-            if (varX < 0 && varY < 0)
-            {
-                Console.WriteLine("Номер координатной четверти: III");
-            }
-            if (varX > 0 && varY < 0)
-            {
-                Console.WriteLine("Номер координатной четверти: IV");
-            }
+            Console.WriteLine("Номер координатной четверти: IV");
         }
 
     }
+
 }
--- HomeWork/ToSeminar2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ HomeWork/ToSeminar2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e6bc14acd4821195a242eefa277ca5ef810171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d9035d555f814332b432934434e689b7ea8d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
