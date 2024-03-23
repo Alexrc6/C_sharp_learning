d905c2716ad7be487f8f36cb7c51fe50bf0d0f12
alex_rc6 <sasha.rc6@gmail.com>
2024-03-23 23:53:19 +0500

    Added Lesson3:Task2 and Task3

---
+++ Lessons/Lesson3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintSquaresAllNumbersinRow(int Num)
+{
+    int i = 1;
+    while (i <= Num)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSquaresAllNumbersinRow(5);
+Console.WriteLine();
+PrintSquaresAllNumbersinRow(10);
+Console.WriteLine();
+PrintSquaresAllNumbersinRow(15);
