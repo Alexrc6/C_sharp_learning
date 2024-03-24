057128bbfa967adb6a430de40f9071b86fdbdba3
alex_rc6 <sasha.rc6@gmail.com>
2024-03-24 22:12:17 +0500

    Added Ref and Out codes in Code_samples

---
+++ Code_samples/4.Input_output/1.Ref/Program.cs
@@ -0,0 +1,22 @@
+﻿void Increment(ref int n)
+{
+    n++;
+    Console.WriteLine($"Число в методе Increment: {n}");
+}
+ 
+int number = 5;
+Console.WriteLine($"Число до метода Increment: {number}");
+Increment(ref number);
+Console.WriteLine($"Число после метода Increment: {number}");
+
+
+
+// void Increment(ref readonly int n)
+// {
+//     // n++; // нельзя, иначе будет очишка компиляции
+//     Console.WriteLine($"Число в методе Increment: {n}");
+// }
+  
+// int number = 5;
+// Increment(ref number);
+// Console.WriteLine($"Число после метода Increment: {number}");
---
+++ Code_samples/4.Input_output/2.Output/Program.cs
@@ -0,0 +1,39 @@
+﻿void Sum(int x, int y, out int result)
+{
+    result = x + y;
+}
+ 
+int number;
+ 
+Sum(10, 15, out number);
+ 
+Console.WriteLine(number);
+
+
+// void GetRectangleData(int width, int height, out int rectArea, out int rectPerimetr)
+// {
+//     rectArea = width * height;       // площадь прямоугольника - произведение ширины на высоту
+//     rectPerimetr = (width + height) * 2; // периметр прямоугольника - сумма длин всех сторон  
+// }
+ 
+// int area;
+// int perimetr;
+ 
+// GetRectangleData(10, 20, out area, out perimetr);
+ 
+// Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+// Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+
+
+
+
+// void GetRectangleData(int width, int height, out int rectArea, out int rectPerimetr)
+// {
+//     rectArea = width * height;  
+//     rectPerimetr = (width + height) * 2; 
+// }
+ 
+// GetRectangleData(10, 20, out int area, out int perimetr);
+ 
+// Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+// Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
